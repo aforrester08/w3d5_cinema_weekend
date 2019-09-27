8f0125ea0cc4b7472c0572c7ad2b11a2f90bb9c9
Alex Forrester <alexforrester96@gmail.com>
2019-09-27 14:04:19 +0100

    added update to all classes

--- models/customer.rb
+++ models/customer.rb
@@ -21,6 +21,13 @@ class Customer
     @id = customer['id'].to_i()
   end
 
+  def update()
+    sql = "UPDATE customers SET (name, funds)
+    = ($1, $2) WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all()
     sql = "SELECT * FROM customers"
     customers = SqlRunner.run(sql)
